e7349fe4217353fd02b6beb2fa935f043b389b93
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-11-06 10:55:19 +0000

    Bot main init

---
+++ HomeMain.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import os
+import discord
+from discord.ext import commands
+import sys
+
+"""
+Discord Home Bot in Python
+
+By Joseph Libasora
+Last updated: --.--.2020, Python 3.8.5
+"""
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+   """Reports when main bot is ready"""
+   # print("AviationBot main ready")
+
+@client.command()
+async def ping(ctx):
+   """Returns latency between bot and server"""
+   await ctx.send(f"Pong main. {round(client.latency * 1000)}ms")
+
+@client.command()
+async def load(ctx, path):
+   """Loads cogs"""
+   client.load_extension(f"cogs.{path}")
+   await ctx.send(f"Cog: {path} loaded")
+
+@client.command()
+async def unload(ctx, path):
+   """Unloads cogs"""
+   client.unload_extension(f"cogs.{path}")
+   await ctx.send(f"Cog: {path} unloaded")
+
+@client.command()
+async def reload(ctx, path):
+   """Reloads cog"""
+   client.unload_extension(f"cogs.{path}")
+   client.load_extension(f"cogs.{path}")
+   await ctx.send(f"Cog: {path} reloaded")
+
+for file_n in os.listdir("./cogs"):
+   if file_n.endswith(".py"):
+      client.load_extension(f"cogs.{file_n[:-3]}")
+
+def main():
+   """
+   Bot token management
+   
+   Note: 
+    - Bot token is kept in the '.token.txt' file
+    - This is to avoid the sharing of bot tokens
+   """
+   global token
+   try:
+      with open("./.token.txt") as fd:
+         token = fd.read().strip()
+   except FileNotFoundError:
+      sys.exit("Warning, Error occured. No '.token.txt' file found!")
+
+if __name__ == "__main__":
+   main()
+   client.run(token)
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Home utility bot using Discord and Python
 
 ## WIP
+
+https://www.flaticon.com/free-icon/home_553416?term=home&page=1&position=45
+Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
