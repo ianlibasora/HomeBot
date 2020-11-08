efb4a268099d66f12fa304a3376cb12155122875
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-11-08 17:17:30 +0000

    Added SyncMETAR()

--- cogs/aptWX.py
+++ cogs/aptWX.py
@@ -2,6 +2,8 @@
 
 import discord
 from discord.ext import commands
+from bs4 import BeautifulSoup
+import requests
 
 class Weather(commands.Cog):
    """
@@ -18,5 +20,30 @@ class Weather(commands.Cog):
    async def WXping(self, ctx):
       await ctx.send("Pong aptWX")
 
+   @staticmethod
+   def SyncMETAR(apt="EIDW"):
+      """SYNC Web request airport METAR data"""
+
+      url = f"https://aviationweather.gov/metar/data?ids={apt}"
+      web = requests.get(url, timeout=10)
+      if web.ok:
+         soup = BeautifulSoup(web.text, "html.parser")
+         try:
+            return soup.code.text
+         except AttributeError:
+            return "Invalid Airport Code"
+      else:
+         return f"Warning, web error occured. Code: {web.status_code}"
+
 def setup(client):
    client.add_cog(Weather(client))
+
+def main():
+   """Local main() for testing"""
+   a = Weather.SyncMETAR("EGLL")
+   b = Weather.SyncMETAR()
+   print(a)
+   print(b)
+
+if __name__ == "__main__":
+   main()
