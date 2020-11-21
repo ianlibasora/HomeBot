8f8f527511b4a57a646aebbbbc30f4be025fb040
Joseph L <56083316+ianlibasora@users.noreply.github.com>
2020-11-21 10:26:38 +0000

    Added GetDay()

--- cogs/times.py
+++ cogs/times.py
@@ -13,7 +13,7 @@ class Timetable(commands.Cog):
    Adapted for discord
    
    By Joseph Libasora
-   Last updated: 15.Nov.2020
+   Last updated: 21.Nov.2020
    """
    def __init__(self, client):
       self.client = client
@@ -41,6 +41,24 @@ class Timetable(commands.Cog):
          # Fail condition: None
          return None
 
+   @staticmethod
+   async def GetDay(i_day=None):
+      """Get the current or requested day"""
+      week = {
+         "mon": "monday", "monday": "monday",
+         "tues": "tuesday", "tue": "tuesday", "tuesday": "tuesday",
+         "wed": "wednesday", "wednesday": "wednesday",
+         "thurs": "thursday", "thursday": "thursday",
+         "fri": "friday", "friday": "friday",
+         "sat": "saturday", "saturday": "saturday",
+         "sun": "sunday", "sunday": "sunday"
+      }
+      if i_day is not None and i_day in week:
+         day = week[i_day.lower()]
+      else:
+         day = calendar.day_name[date.today().weekday()].lower()
+      return day
+
 def setup(client):
    client.add_cog(Timetable(client))
 
@@ -51,8 +69,10 @@ def main():
 async def amain():
    """Local testing amain()"""
    # table = Timetable()
-   code = await Timetable.GetTable()
-   print(code)
+   # code = await Timetable.GetTable()
+   # print(code)
+   day = await Timetable.GetDay("a")
+   print(day)
 
 if __name__ == "__main__":
    # main()
