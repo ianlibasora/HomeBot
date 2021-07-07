e4a40b2b00c52334eefdc8b2e845f8ee5597e9d4
Joseph Libasora <56083316+ianlibasora@users.noreply.github.com>
2021-07-07 11:31:51 +0100

    Added F1 WDC get data function

--- cogs/f1.py
+++ cogs/f1.py
@@ -20,6 +20,36 @@ class F1(commands.Cog):
    async def F1ping(self, ctx):
       await ctx.send("Pong f1.py")
 
+   @staticmethod
+   async def getWDC():
+      """Async web request F1 World Driver's Championship standings"""
+
+      url = "https://www.skysports.com/f1/standings"
+      timeout = aiohttp.ClientTimeout(total=10)
+      async with aiohttp.ClientSession(timeout=timeout) as sesh:
+         async with sesh.get(url) as web_resp:
+            if web_resp.status == 200:
+               web = await web_resp.text()
+               soup = BeautifulSoup(web, "html.parser")
+               wdc = []
+               
+               for driver in soup.find_all("table")[0].find_all("tr")[1:]:
+                  wdc.append([driverdata.text.strip() for driverdata in driver.find_all("td")])
+               return wdc
+            else:
+               return "Warning, web error occured. Code: {web_resp.status}"
+      return "Warning, request timeout"
 
 def setup(client):
    client.add_cog(F1(client))
+
+
+async def asyncTest():
+   """Local async testing"""
+   print("testing async local functions")
+
+   print(await F1.getWDC())
+
+if __name__ == "__main__":
+   import asyncio
+   asyncio.run(asyncTest())
