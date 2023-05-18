263c34c6c1c94b182002e530073c17f447267039
Joseph Libasora <56083316+ianlibasora@users.noreply.github.com>
2023-05-18 15:52:40 +0100

    Updated HomeMain

--- HomeMain.py
+++ HomeMain.py
@@ -1,102 +1,118 @@
 #!/usr/bin/env python3
 
-import os
+from os import getenv
+from pathlib import Path
+
 import discord
 from discord.ext import commands
-from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 """
-Discord Home Bot in Python
+Discord HomeBot
 
-By Joseph Libasora
-Last updated: 20.MAR.2021
+Joseph Libasora
+Last updated: 18.MAY.2023
 """
 
-client = commands.Bot(command_prefix="!")
+
+BASE_DIR_PATH = Path(__file__).parent
+COGS_DIR = "cmds"
+COGS_DIR_PATH = BASE_DIR_PATH.joinpath(COGS_DIR)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="!", intents=intents)
 client.remove_command("help")
+
 
 @client.event
 async def on_ready():
-   """Reports when main bot is ready"""
+    """Reports when main bot is ready"""
+
+    for file in COGS_DIR_PATH.iterdir():
+        if str(file).endswith(".py"):
+            await client.load_extension(f"{COGS_DIR}.{file.name[:-3]}")
+    print("HomeBot activated")
 
 
 @client.command()
 async def ping(ctx):
-   """Returns latency between bot and server"""
-   await ctx.send(f"Pong main. {round(client.latency * 1000)}ms")
+    """Returns latency between bot and server"""
+    await ctx.send(f"Pong. {round(client.latency * 1000)}ms")
 
 
 @client.group(invoke_without_command=True)
 async def help(ctx):
-   """Custom bot help command"""
+    """Custom bot help command"""
 
-   embed = discord.Embed(
-      title="HomeBot Help Menu",
-      description="Bot command prefix: !",
-      colour=discord.Colour.blue()
-   )
-   # aptWX.py
-   embed.add_field(name="!metar [airport_code]", value="> METAR report for a given airport", inline=True)
-   embed.add_field(name="!taf [airport_code]", value="> TAF report for a given airport", inline=True)
-   embed.add_field(name="!wx [airport_code]", value="> Full METAR/TAF report for a given airport", inline=True)
+    embed = discord.Embed(
+        title="HomeBot Help Menu",
+        description="Bot command prefix: !",
+        colour=discord.Colour.blue()
+    )
 
-   # times.py
-   embed.add_field(name="!time [day]", value="> Request class timetable", inline=True)
-   
-   # covid19.py
-   embed.add_field(name="!covid", value="> Request Ireland COVID-19 Data", inline=True)
+    # # aptWX.py
+    # embed.add_field(name="!metar [airport_code]", value="> METAR report for a given airport", inline=True)
+    # embed.add_field(name="!taf [airport_code]", value="> TAF report for a given airport", inline=True)
+    # embed.add_field(name="!wx [airport_code]", value="> Full METAR/TAF report for a given airport", inline=True)
 
-   # f1.py
-   embed.add_field(name="!f1", value="> Request F1 (Full) Schedule", inline=True)
-   embed.add_field(name="!f1.next", value="> Request F1 (Next) Schedule", inline=True)
-   embed.add_field(name="!wdc", value="> Request F1 WDC Standings", inline=True)
-   embed.add_field(name="!wcc", value="> Request F1 WCC Standings", inline=True)
+    # # times.py
+    # embed.add_field(name="!time [day]", value="> Request class timetable", inline=True)
+    
+    # # covid19.py
+    # embed.add_field(name="!covid", value="> Request Ireland COVID-19 Data", inline=True)
 
-   embed.set_thumbnail(url="https://raw.githubusercontent.com/ianlibasora/HomeBot/master/images/home.png")
-   embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author}")
-   embed.set_author(name="HomeBot", icon_url="https://raw.githubusercontent.com/ianlibasora/HomeBot/master/images/home.png")
-   await ctx.send(embed=embed)
+    # # f1.py
+    # embed.add_field(name="!f1", value="> Request F1 (Full) Schedule", inline=True)
+    # embed.add_field(name="!f1.next", value="> Request F1 (Next) Schedule", inline=True)
+    # embed.add_field(name="!wdc", value="> Request F1 WDC Standings", inline=True)
+    # embed.add_field(name="!wcc", value="> Request F1 WCC Standings", inline=True)
+
+    embed.set_thumbnail(url="https://raw.githubusercontent.com/ianlibasora/HomeBot/master/images/home.png")
+    embed.set_footer(icon_url=ctx.author.avatar.url, text=f"Requested by {ctx.author}")
+    embed.set_author(name="HomeBot", icon_url="https://raw.githubusercontent.com/ianlibasora/HomeBot/master/images/home.png")
+    await ctx.send(embed=embed)
 
 
 @client.command()
 async def load(ctx, path):
-   """Loads cogs"""
-   client.load_extension(f"cogs.{path}")
-   await ctx.send(f"Cog: {path} loaded")
+    """Loads cogs"""
+
+    await client.load_extension(f"{COGS_DIR}.{path}")
+    await ctx.send(f"Cog ({path}) loaded")
 
 
 @client.command()
 async def unload(ctx, path):
-   """Unloads cogs"""
-   client.unload_extension(f"cogs.{path}")
-   await ctx.send(f"Cog: {path} unloaded")
+    """Unloads cogs"""
+
+    await client.unload_extension(f"{COGS_DIR}.{path}")
+    await ctx.send(f"Cog ({path}) unloaded")
 
 
 @client.command()
 async def reload(ctx, path):
-   """Reloads cog"""
-   client.unload_extension(f"cogs.{path}")
-   client.load_extension(f"cogs.{path}")
-   await ctx.send(f"Cog: {path} reloaded")
+    """Reloads cog"""
+
+    await client.unload_extension(f"{COGS_DIR}.{path}")
+    await client.load_extension(f"{COGS_DIR}.{path}")
+    await ctx.send(f"Cog ({path}) reloaded")
 
 
 @client.command()
 async def freload(ctx):
-   """Reload all cogs"""
-   for file_n in os.listdir("./cogs"):
-      if file_n.endswith(".py"):
-         client.unload_extension(f"cogs.{file_n[:-3]}")
-         client.load_extension(f"cogs.{file_n[:-3]}")
-   await ctx.send("Reloaded all cogs")
+    """Reload all cogs"""
 
-for file_n in os.listdir("./cogs"):
-   if file_n.endswith(".py"):
-      client.load_extension(f"cogs.{file_n[:-3]}")
+    for file in COGS_DIR_PATH.iterdir():
+        if str(file).endswith(".py"):
+            await client.unload_extension(f"{COGS_DIR}.{file.name[:-3]}")
+            await client.load_extension(f"{COGS_DIR}.{file.name[:-3]}")
+    await ctx.send("Reloaded all cogs")
 
 
 if __name__ == "__main__":
-   """
-   Bot token management
-   Bot token is handled through environment variables
-   """
-   client.run(config("TOKEN"))
+    client.run(getenv("TOKEN"))
